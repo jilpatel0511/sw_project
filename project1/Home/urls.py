b579c32e92ec8004b7caaa78dcf4817c9be476a8
Jil Patel <jilpatel0511@gmail.com>
2021-06-08 10:02:43 +0530

    Login page created

--- project1/Home/urls.py
+++ project1/Home/urls.py
@@ -3,4 +3,5 @@ from .views import *
 urlpatterns = [
    path('index/',Index),
    path('common/',Common),
+   path('login/',login),
 ]
--- project1/Home/views.py
+++ project1/Home/views.py
@@ -6,3 +6,6 @@ def Index(req):
 
 def Common(req):
     return render(req,'common.html')
+
+def login(req):
+    return render(req,'login.html')
